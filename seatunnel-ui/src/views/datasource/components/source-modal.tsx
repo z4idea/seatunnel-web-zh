061cdf9b651acc870f371759284e10e9d4c76d2e
yaoyuan <1537693643@qq.com>
2026-05-28 14:45:33 +0800

    修改弹窗样式

--- seatunnel-ui/src/views/datasource/components/source-modal.tsx
+++ seatunnel-ui/src/views/datasource/components/source-modal.tsx
@@ -18,20 +18,19 @@
  * limitations under the License.
  */
 
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, PropType, ref, watch, h } from 'vue'
 import {
   NSpace,
   NModal,
   NCard,
   NButton,
-  NTabs,
-  NTabPane,
   NEmpty
 } from 'naive-ui'
 import { useI18n } from 'vue-i18n'
 import { useSource } from './use-source'
 import styles from './source-model.module.scss'
 import type { SelectOption } from 'naive-ui'
+import TitleIcon from '@/assets/title-icon.png'
 
 const props = {
   show: {
@@ -50,39 +49,123 @@ const SourceModal = defineComponent({
   setup(props, ctx) {
     const { t } = useI18n()
     const { state } = useSource(false)
+    const activeTab = ref(state.types[0]?.key || '')
+    
     const handleTypeSelect = (type: string) => {
       ctx.emit('change', type)
     }
     const onCancel = () => {
       ctx.emit('cancel')
     }
+    const handleTabChange = (key: string) => {
+      activeTab.value = key
+    }
+
+    watch(
+      () => props.show,
+      (show) => {
+        if (show && state.types.length > 0) {
+          activeTab.value = state.types[0].key
+        }
+      }
+    )
 
     return () => (
-      <NModal show={props.show} onMaskClick={onCancel} onEsc={onCancel}>
+      <NModal show={props.show} onMaskClick={onCancel} onEsc={onCancel} style={{ width: '1000px' }}>
         <NCard
           class={styles.content}
-          title={t('datasource.choose_datasource_type')}
+          title={h('div', { 
+            style: { 
+              display: 'flex', 
+              alignItems: 'center', 
+              gap: '8px',
+              color: '#2C3947',
+              fontWeight: 600,
+              fontFamily: 'PingFang SC',
+              fontSize: '24px',
+              paddingBottom: '12px',
+              borderBottom: '1px solid #DFE3E8'
+            } 
+          }, [
+            h('img', { src: TitleIcon, style: { width: '35px', height: '35px' } }),
+            h('span', t('datasource.choose_datasource_type'))
+          ])}
         >
-          <NTabs>
-            {state.types.map((item) => (
-              <NTabPane name={item.key} tab={item.label} key={item.key}>
-                <div class={styles['types']}>
-                  {item?.children.map((slip: SelectOption) => (
-                    <div
-                      class={styles.itemBox}
-                      onClick={() => handleTypeSelect(slip.value as string)}
-                    >
-                      <div class='font-bold'>{t(slip.label as string)}</div>
-                      <div class='text-xs mt-1.5'>{item.label}</div>
-                    </div>
-                  ))}
+          <div style={{ display: 'flex', height: '500px' }}>
+            {/* 左侧 tab 导航 */}
+            <div style={{ width: '160px', borderRight: '1px dashed #DDE1ED', paddingTop: '16px' }}>
+              {state.types.map((item) => (
+                <div
+                  key={item.key}
+                  onClick={() => handleTabChange(item.key)}
+                  style={{
+                    padding: '16px',
+                    cursor: 'pointer',
+                    backgroundColor: activeTab.value === item.key ? '#ffffff' : 'transparent',
+                    color: activeTab.value === item.key ? '#1960BC' : '#4b5563',
+                    fontWeight: activeTab.value === item.key ? 500 : 400,
+                    display: 'flex',
+                    alignItems: 'center',
+                    position: 'relative',
+                    fontSize: '16px',
+                    fontFamily: 'PingFang SC'
+                  }}
+                >
+                  {activeTab.value === item.key && (
+                    <div style={{
+                      position: 'absolute',
+                      left: '0px',
+                      width: '4px',
+                      height: '20px',
+                      backgroundColor: '#1960BC',
+                      borderRadius: '2px'
+                    }} />
+                  )}
+                  {item.label}
                 </div>
-                {item.children.length === 0 && <NEmpty />}
-              </NTabPane>
-            ))}
-          </NTabs>
+              ))}
+            </div>
+
+            {/* 右侧内容区域 */}
+            <div style={{ flex: 1, paddingLeft: '16px', overflow: 'auto', position: 'relative', zIndex: 1 }}>
+              {state.types.map((item) => (
+                activeTab.value === item.key && (
+                  <div key={item.key}>
+                    <div class={styles['types']}>
+                      {item?.children.map((slip: SelectOption) => (
+                        <div
+                          key={slip.value as string}
+                          class={styles.itemBox}
+                          onClick={() => handleTypeSelect(slip.value as string)}
+                        >
+                          <div style={{color:'#303133',fontWeight:'500',fontSize:'18px'}}>{t(slip.label as string)}</div>
+                          <div class='text-xs mt-3' style={{color:'#909399',fontSize:'14px'}}>{item.label}</div>
+                        </div>
+                      ))}
+                    </div>
+                    {item.children.length === 0 && <NEmpty />}
+                  </div>
+                )
+              ))}
+            </div>
+          </div>
           <NSpace justify='end'>
-            <NButton onClick={onCancel}>{t('datasource.cancel')}</NButton>
+            <NButton 
+              secondary 
+              onClick={onCancel}
+              style={{ 
+                
+                width: '80px', 
+                height: '44px', 
+                backgroundColor: '#F3F7FB', 
+                color: '#1960BC',
+                borderRadius:"4px",
+                fontWeight:'600',
+                fontFamily: 'PingFang SC'
+              }}
+            >
+              {t('datasource.cancel')}
+            </NButton>
           </NSpace>
         </NCard>
       </NModal>
